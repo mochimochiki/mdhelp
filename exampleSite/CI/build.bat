df5e50a29cb4cdc3f605ba00dd4cc504ce1c7565
mochimo <mochikimoto@yahoo.co.jp>
2020-07-13 20:53:43 +0900

    build.batなどを英語対応。

--- exampleSite/CI/build.bat
+++ exampleSite/CI/build.bat
@@ -1,20 +1,35 @@
 @echo off
 pushd %~dp0..
 
+:: -----------------------
+:: Normal Edition
+:: -----------------------
 echo Build Normal edition...
 hugo --environment chm
 if not %errorlevel% == 0 exit /B 1
 
+:: JP Help
 hhc.exe public_chm\jp\project.hhp
-if not %errorlevel% == 1 exit /B 1
 :: hhc.exe returns 1 on success  https://ja.coder.work/so/cmd/1267049
+if not %errorlevel% == 1 exit /B 1
 
+:: EN Help
+hhc.exe public_chm\en\project.hhp
+if not %errorlevel% == 1 exit /B 1
+
+:: -----------------------
+:: Other Edition
+:: -----------------------
 echo Build some directory ignored edition...
 hugo --environment chm_other_version
 if not %errorlevel% == 0 exit /B 1
 
+:: JP Help
 hhc.exe public_chm_other_version\jp\project.hhp
 if not %errorlevel% == 1 exit /B 1
-:: hhc.exe returns 1 on success  https://ja.coder.work/so/cmd/1267049
+
+:: EN Help
+hhc.exe public_chm_other_version\en\project.hhp
+if not %errorlevel% == 1 exit /B 1
 
 popd
