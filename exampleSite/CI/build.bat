e767052a47cbd9e27aae2cacbb593b789f979328
mochimo <mochikimoto@yahoo.co.jp>
2020-07-20 23:53:27 +0900

    powershell実行時のポリシー設定

--- exampleSite/CI/build.bat
+++ exampleSite/CI/build.bat
@@ -12,7 +12,7 @@ if not %errorlevel% == 0 exit /B 1
 :: JP Help
 :: ----------
 :: convert to sjis
-powershell ".\CI\UTF8toSJIS.ps1 -dir public_chm\jp -logname unknown_chm.log;exit $LASTEXITCODE"
+powershell -NoProfile -ExecutionPolicy RemoteSigned ".\CI\UTF8toSJIS.ps1 -dir public_chm\jp -logname unknown_chm.log;exit $LASTEXITCODE"
 if not %errorlevel% == 0 exit /B 1
 hhc.exe public_chm\jp\project.hhp
 :: hhc.exe returns 1 on success  https://ja.coder.work/so/cmd/1267049
@@ -34,7 +34,7 @@ if not %errorlevel% == 0 exit /B 1
 :: ----------
 :: JP Help
 :: ----------
-powershell ".\CI\UTF8toSJIS.ps1 -dir public_chm_other_version\jp -logname unknown_other.log;exit $LASTEXITCODE"
+powershell -NoProfile -ExecutionPolicy RemoteSigned ".\CI\UTF8toSJIS.ps1 -dir public_chm_other_version\jp -logname unknown_other.log;exit $LASTEXITCODE"
 if not %errorlevel% == 0 exit /B 1
 hhc.exe public_chm_other_version\jp\project.hhp
 if not %errorlevel% == 1 exit /B 1
