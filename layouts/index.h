d7351019a4f35fbfb93b2c757fe3095be3d3ac25
mochimo <mochikimoto@yahoo.co.jp>
2021-04-12 23:42:29 +0900

    fix: html以外のoutputformatはhelp設定ファイルに含まれないようにした

--- layouts/index.h
+++ layouts/index.h
@@ -1,11 +1,13 @@
 {{- define "create_alias" -}}
 {{- range .Pages -}}
+{{- if .OutputFormats.Get "html" }}
 {{- $id := replace .RelPermalink "/" "_" -}}
 {{- $id = replace $id ".html" "" | upper -}}
 #define IDH_{{ $id }} xxxxx
 {{ chomp "\r\n"}}
 {{- if .IsSection -}}
 {{ template "create_alias" . }}
+{{- end -}}
 {{- end -}}
 {{- end -}}
 {{- end -}}
